0aea9954126b46bbf6cedfad65008ead1bd986a6
Mauricio Valer <65901444+mlvaler@users.noreply.github.com>
2020-08-07 16:43:36 -0300

    Update prediction

--- prediction.R
+++ prediction.R
@@ -1,14 +1,10 @@
-prediction <- function(month_proj) 
-  {
-  print(month_proj)
+prediction <- function(month_proj) {
   value.m1.ref <- drc::drm(
     value.ref.real_2 ~ AUX.period.ref,
     weights = AUX.weights.ref.real,
     robust = "tukey", #mean
     type = "continuous",
     fct = LL2.3())
-  
   value.ref.predict <- predict(value.m1.ref, data.frame(AUX.period = (max(AUX.period.ref) + 1): month_proj))
-  
   return(value.ref.predict)
 }
